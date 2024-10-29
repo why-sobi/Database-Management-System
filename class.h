186a79a9e958820f4ae359fd8fcc72e891361ab5
why-sobi <sobisoban05@gmail.com>
2024-10-29 22:32:43 +0500

    Extended Some Classes Functionality

--- class.h
+++ class.h
@@ -1,27 +1,34 @@
 #include <iostream>
+#include "header.h"
 
 template <typename T>
-class Array {
-    T* arr;
+class Array
+{
+    T *arr;
     int size;
     int index;
 
-    void increaseSize(int newSize) {
-        T* newArr = new T[newSize];
-        for (int i = 0; i < this->size; i++) {
+    void increaseSize(int newSize)
+    {
+        T *newArr = new T[newSize];
+        for (int i = 0; i < this->size; i++)
+        {
             newArr[i] = this->arr[i];
         }
-        delete[] this->arr; 
+        delete[] this->arr;
         this->arr = newArr;
         this->size = newSize;
     }
 
-    void allocate(int Size, bool keepValue = false) {
-        if (keepValue) {
+    void allocate(int Size, bool keepValue = false)
+    {
+        if (keepValue)
+        {
             increaseSize(Size);
             return;
         }
-        if (arr) {
+        if (arr)
+        {
             delete[] arr;
         }
         arr = new T[Size];
@@ -31,69 +38,86 @@ class Array {
 
 public:
     Array() : size(0), index(0), arr(nullptr) {}
-    
-    ~Array() {
-        if (arr) {
-            delete[] arr;  
+
+    ~Array()
+    {
+        if (arr)
+        {
+            delete[] arr;
             arr = nullptr;
         }
     }
-    
+
     // Fixed copy constructor
-    Array(const Array& other) : size(other.size), index(other.index), arr(nullptr) {
+    Array(const Array &other) : size(other.size), index(other.index), arr(nullptr)
+    {
         std::cout << "Copy Constructor Called\n";
-        if (other.arr) {
+        if (other.arr)
+        {
             arr = new T[size];
-            for (int i = 0; i < size; i++) {  // Copy all elements up to size
+            for (int i = 0; i < size; i++)
+            { // Copy all elements up to size
                 arr[i] = other.arr[i];
             }
         }
     }
 
     // Methods
-    void add(const T& value) {
-        if (size == 0) {
+    void add(const T &value)
+    {
+        if (size == 0)
+        {
             allocate(1);
         }
-        if (index >= size) {  // if we need to resize
+        if (index >= size)
+        { // if we need to resize
             allocate(size * 2, true);
         }
         arr[index++] = value;
     }
 
-    T pop(int pos = -1) {
-        
-        if (pos == -1) {
+    T pop(int pos = -1)
+    {
+
+        if (pos == -1)
+        {
             pos = index - 1;
         }
         T value = arr[pos];
-        for (int i = pos; i < index - 1; i++) {
+        for (int i = pos; i < index - 1; i++)
+        {
             arr[i] = arr[i + 1];
         }
         index--;
         return value;
     }
 
-    Array& operator=(const Array& other) {
-        if (this != &other) { 
-            if (arr) {
+    Array &operator=(const Array &other)
+    {
+        if (this != &other)
+        {
+            if (arr)
+            {
                 delete[] arr;
             }
             size = other.size;
             index = other.index;
             arr = new T[size];
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++)
+            {
                 arr[i] = other.arr[i];
             }
         }
-        return *this; 
+        return *this;
     }
 
-    const T& operator[](int i) const { 
+    const T &operator[](int i) const
+    {
         return arr[i];
     }
 
-    T& operator[](int i) { 
+    T &operator[](int i)
+    {
         return arr[i];
     }
 
@@ -101,7 +125,8 @@ public:
     int getCapacity() const { return size; }
 };
 
-class GamesPlayedNode {
+class GamesPlayedNode
+{
 private:
     std::string ID;
     float hrsPlayed;
@@ -111,53 +136,149 @@ public:
     GamesPlayedNode() : ID(""), hrsPlayed(0.0f), achievements(0) {}
 
     std::string getID() const { return ID; }
-    void setID(const std::string& id) { ID = id; }
+    void setID(const std::string &id) { ID = id; }
 
     float getHrsPlayed() const { return hrsPlayed; }
     void setHrsPlayed(float hours) { hrsPlayed = hours; }
 
     int getAchievements() const { return achievements; }
     void setAchievements(int ach) { achievements = ach; }
+
+
+    friend std::ostream &operator<<(std::ostream &out, GamesPlayedNode &obj)
+    {
+        out << "ID: " << obj.ID << '\n';
+        out << "Hours Played: " << obj.hrsPlayed << '\n';
+        out << "Acheivements Unlocked: " << obj.achievements << '\n';
+        out << '\n';
+        return out;
+    }
 };
 
-class Player {
+class Player
+{
 private:
     std::string ID;
     std::string name;
     std::string number;
     std::string email;
     std::string password;
-    Array<GamesPlayedNode> gamesPlayed;
+    BST<GamesPlayedNode> gamesPlayed;
 
 public:
     Player() : ID(""), name(""), number(""), email(""), password("") {}
 
+    // Getters and Setters
     std::string getID() const { return ID; }
-    void setID(const std::string& id) { ID = id; }
+    void setID(const std::string &id) { ID = id; }
 
     std::string getName() const { return name; }
-    void setName(const std::string& playerName) { name = playerName; }
+    void setName(const std::string &playerName) { name = playerName; }
 
     std::string getNumber() const { return number; }
-    void setNumber(const std::string& playerNumber) { number = playerNumber; }
+    void setNumber(const std::string &playerNumber) { number = playerNumber; }
 
     std::string getEmail() const { return email; }
-    void setEmail(const std::string& playerEmail) { email = playerEmail; }
+    void setEmail(const std::string &playerEmail) { email = playerEmail; }
 
     std::string getPassword() const { return password; }
-    void setPassword(const std::string& playerPassword) { password = playerPassword; }
+    void setPassword(const std::string &playerPassword) { password = playerPassword; }
 
-    GamesPlayedNode& operator [] (int index) {
-        return gamesPlayed[index];
+    // Methods
+    bool edit()
+    {
+        std::cout << "What do you want to edit: ";
+        std::cout << "1. ID\n";
+        std::cout << "2. Name\n";
+        std::cout << "3. Number\n";
+        std::cout << "4. Email\n";
+        std::cout << "5. Password\n";
+        std::cout << "6. Specific Game\n";
+        std::cout << "Enter Choice: ";
+        char choice;
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case '1':
+            std::cout << "Enter new ID: ";
+            std::cin >> ID;
+            break;
+        case '2':
+            std::cout << "Enter new Name: ";
+            std::cin >> name;
+            break;
+        case '3':
+            std::cout << "Enter new Number: ";
+            std::cin >> number;
+            break;
+        case '4':
+            std::cout << "Enter new Email: ";
+            std::cin >> email;
+            break;
+        case '5':
+            std::cout << "Enter new Password: ";
+            std::cin >> password;
+            break;
+        case '6':
+            std::string gameID;
+            std::cout << "Enter Game ID: ";
+            std::cin >> gameID;
+        }
+        return choice == '1';
     }
 
-    const GamesPlayedNode& operator [] (int index) const {
-        return gamesPlayed[index];
+    // Operator overload
+    bool operator<(const Player &other)
+    {
+        return this->ID < other.ID;
+    }
+
+    bool operator>(const Player &other)
+    {
+        return this->ID > other.ID;
+    }
+
+    bool operator==(const Player &other)
+    {
+        return this->ID == other.ID;
+    }
+
+    bool operator<(const std::string &other)
+    {
+        return this->ID < other;
+    }
+
+    bool operator>(const std::string &other)
+    {
+        return this->ID > other;
+    }
+
+    bool operator==(const std::string &other)
+    {
+        return this->ID == other;
+    }
+
+    void operator = (Player& obj) {
+        *this = obj;
+        this->gamesPlayed.moveOwnerShip(obj.gamesPlayed);
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, Player &obj)
+    {
+        out << "------------ " << obj.name << " has the following details------------";
+        out << "ID: " << obj.ID << '\n';
+        out << "Contact Number: " << obj.number << '\n';
+        out << "Email: " << obj.email << '\n';
+        out << "Password: " << obj.password << '\n';
+        out << "Games played: " << obj.gamesPlayed << '\n';
+        out << '\n';
+        return out;
     }
 };
 
-
-class Game {
+class Game
+{
 private:
     std::string ID;
     std::string name;
@@ -167,24 +288,66 @@ private:
     int downloads;
 
 public:
-    Game(const std::string& id, const std::string& name, const std::string& developer, const std::string& publisher, float size, int downloads)
+    Game(const std::string &id, const std::string &name, const std::string &developer, const std::string &publisher, float size, int downloads)
         : ID(id), name(name), developer(developer), publisher(publisher), sizeInGBs(size), downloads(downloads) {}
 
     std::string getID() const { return ID; }
-    void setID(const std::string& id) { ID = id; }
+    void setID(const std::string &id) { ID = id; }
 
     std::string getName() const { return name; }
-    void setName(const std::string& name) { this->name = name; }
+    void setName(const std::string &name) { this->name = name; }
 
     std::string getDeveloper() const { return developer; }
-    void setDeveloper(const std::string& developer) { this->developer = developer; }
+    void setDeveloper(const std::string &developer) { this->developer = developer; }
 
     std::string getPublisher() const { return publisher; }
-    void setPublisher(const std::string& publisher) { this->publisher = publisher; }
+    void setPublisher(const std::string &publisher) { this->publisher = publisher; }
 
     float getSizeInGBs() const { return sizeInGBs; }
     void setSizeInGBs(float size) { sizeInGBs = size; }
 
     int getDownloads() const { return downloads; }
     void setDownloads(int downloads) { this->downloads = downloads; }
+
+    bool operator<(const Game &other)
+    {
+        return this->ID < other.ID;
+    }
+
+    bool operator==(const Game &other)
+    {
+        return this->ID == other.ID;
+    }
+
+    bool operator>(const Game &other)
+    {
+        return this->ID > other.ID;
+    }
+
+    bool operator<(const std::string &other)
+    {
+        return this->ID < other;
+    }
+
+    bool operator>(const std::string &other)
+    {
+        return this->ID > other;
+    }
+
+    bool operator==(const std::string &other)
+    {
+        return this->ID == other;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Game &obj)
+    {
+        out << "------------ " << obj.name << " has the following details------------";
+        out << "ID: " << obj.ID << '\n';
+        out << "Developer: " << obj.developer << '\n';
+        out << "Pusblisher: " << obj.publisher << '\n';
+        out << "Size(GBs): " << obj.sizeInGBs << '\n';
+        out << "Downloads: " << obj.downloads << '\n';
+        out << '\n';
+        return out;
+    }
 };
--- header.h
+++ header.h
@@ -1,1 +1,323 @@
-#include "class.h"
+#include <iostream>
+
+template <typename T>
+struct Node
+{
+    T data;
+    Node<T> *next;
+
+    Node() { next = nullptr; }
+    Node(const T &val) : data(val) { next = nullptr; }
+};
+
+template <typename T>
+class Queue
+{
+    Node<T> *head;
+    Node<T> *tail;
+
+public:
+    Queue() : head(nullptr), tail(nullptr) {}
+
+    ~Queue()
+    {
+        while (head)
+        {
+            Node<T> *toDel = head;
+            head = head->next;
+            delete toDel;
+        }
+        head = tail = nullptr;
+    }
+    void enqueue(const T &val)
+    {
+        if (head == nullptr)
+        {
+            head = tail = new Node<T>(val);
+            return;
+        }
+        else
+        {
+            tail->next = new Node<T>(val);
+            tail = tail->next;
+            return;
+        }
+    }
+    T dequeue()
+    {
+        Node<T> *temp = head;
+        head = head->next;
+
+        T value = temp->data;
+        delete temp;
+        return value;
+    }
+
+    bool isEmpty()
+    {
+        return head == nullptr;
+    }
+
+    int getSize()
+    {
+        int size = 0;
+        Node<T> *temp = head;
+        while (temp != nullptr)
+        {
+            size++;
+            temp = temp->next;
+        }
+        return size;
+    }
+};
+
+template <typename T>
+struct TreeNode
+{
+    T data;
+    TreeNode *left, *right;
+
+    TreeNode() { left = right = nullptr; }
+    TreeNode(const T &val) : data(val) { left = right = nullptr; }
+};
+
+template <typename T>
+class BST
+{
+    TreeNode<T> *root;
+
+    void destroy(TreeNode<T> *curr)
+    {
+        if (curr)
+        {
+            destroy(curr->left);
+            destroy(curr->right);
+            delete curr;
+        }
+    }
+    TreeNode<T> *findMin(TreeNode<T> *curr)
+    {
+        while (curr->left != NULL)
+            curr = curr->left;
+        return curr;
+    }
+    TreeNode<T> *deleteTreeNode(TreeNode<T> *root, const T &info)
+    {
+        if (root == NULL)
+        { // Tree is empty or element not found
+            return root;
+        }
+        if (info < root->data)
+        { // Traverse left subtree
+            root->left = (deleteTreeNode(root->left, info));
+        }
+        else if (info > (root->data))
+        {
+            // Traverse right subtree
+            root->right = (deleteTreeNode(root->right, info));
+        }
+        else
+        { // TreeNode to be deleted is found
+            // Case 1: TreeNode has no children (it's a leaf)
+            if (root->left == NULL && root->right == NULL)
+            {
+                delete root;
+                return NULL;
+            }
+            // Case 2: TreeNode has one child
+            else if (root->left == NULL)
+            {
+                TreeNode<T> *temp = root->right;
+                delete root;
+                return temp;
+            }
+            else if (root->right == NULL)
+            {
+                TreeNode<T> *temp = root->left;
+                delete root;
+                return temp;
+            }
+            // Case 3: TreeNode has two children
+            TreeNode<T> *temp = findMin(root->right);
+            root->data = (temp->data);
+            root->right = (deleteTreeNode(root->right, temp->data));
+        }
+        return root;
+    }
+    TreeNode<T> *search(TreeNode<T> *root, std::string key)
+    {
+        if (root == NULL || root->data == key)
+            return root;
+        TreeNode<T> *curr = root;
+        while (curr)
+        {
+            if (key < curr->data)
+            {
+                curr = curr->left;
+            }
+            else if (key > curr->data)
+            {
+                curr = curr->right;
+            }
+            else
+            {
+                return curr;
+            }
+        }
+        return nullptr;
+    }
+    void preorderTraversal(TreeNode<T> *curr)
+    {
+        if (curr != nullptr)
+        {
+            std::cout << curr->data << '\n';
+            preorderTraversal(curr->left);
+            preorderTraversal(curr->right);
+        }
+    }
+
+public:
+    BST() { root = nullptr; }
+    ~BST()
+    {
+        destroy(root);
+    }
+
+    void moveOwnerShip(BST<T> &from)
+    {
+        this->root = from.root;
+        from.root = nullptr;
+    }
+
+    // Methods
+    void insert(T &val)
+    {
+        if (!root)
+        {
+            root = new TreeNode<T>(val);
+            return;
+        }
+        TreeNode<T> *curr = root;
+        while (curr)
+        {
+            if (val < curr->data)
+            {
+                if (!curr->left)
+                {
+                    curr->left = new TreeNode<T>(val);
+                    return;
+                }
+                curr = curr->left;
+            }
+            else if (val > curr->data)
+            {
+                if (!curr->right)
+                {
+                    curr->right = new TreeNode<T>(val);
+                    return;
+                }
+                curr = curr->right;
+            }
+            else
+            {
+                std::cout << "Value Already Exists\n";
+                return;
+            }
+        }
+    }
+    void deleteTreeNode(const T &info)
+    {
+        deleteTreeNode(root, info);
+    }
+    TreeNode<T> *search(std::string dataID)
+    {
+        return search(root, dataID);
+    }
+    void showNlayers(int layers)
+    {
+        if (layers < 0)
+            return;
+
+        Queue<Node<T> *> q;
+        q.push(root);
+
+        int currLayer = 0;
+        while (currLayer < layers && !q.isEmpty())
+        {
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                Node<T> *node = q.enqueue();
+                std::cout << *node;
+
+                if (node->left)
+                    q.enqueue(node->left);
+                if (node->right)
+                    q.enqueue(node->right);
+            }
+            ++currLayer;
+        }
+        if (currLayer < layers)
+        {
+            std::cout << "No More Layers\n";
+        }
+    }
+    void findAndEdit(std::string ID)
+    {
+        TreeNode<T> *toDel = search(ID);
+        if (toDel)
+        {
+
+            TreeNode<T> *newNode = TreeNode<T>();
+            newNode = toDel;
+            bool changedID = newNode->edit();
+            if (changedID)
+            {
+                deleteTreeNode(toDel->ID);
+                insertNode(newNode);
+            }
+            else
+            {
+                toDel = newNode;
+            }
+        }
+    }
+
+    // Operator overload
+    friend std::ostream &operator<<(std::ostream &out, BST &obj)
+    {
+        obj.preorderTraversal(obj.root);
+        return out;
+    }
+    // friend std::istream &operator >> (std::istream &in, const BST &obj) {
+
+    // }
+};
+
+// template <typename T>
+// class GamesPlayedBST : public BST<GamesPlayedNode>
+// {
+//     int nodes = 0;
+
+// public:
+//     GamesPlayedBST() : BST<GamesPlayedNode>() {}
+//     void insert(std::string ID)
+//     {
+//         BST<GamesPlayedNode>::insert(ID);
+//         ++nodes;
+//     }
+//     void deleteTreeNode(std::string ID) {
+//         BST<GamesPlayedNode>::deleteTreeNode(ID);
+//         --nodes;
+//     }
+
+//     int getGamesPlayed () const { return nodes; }
+// };
+
+// template <typename T>
+// class PlayerBST : public BST<Player>
+// {
+//     public:
+//     PlayerBST() : BST<Player>() {}
+
+// };
